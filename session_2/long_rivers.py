dcd0f95ddfeef9f08e32e02734d06b8b4e991e3e
Afro-Tyche <afrotyche@gmail.com>
2023-08-19 21:44:06 +0100

    finised session_2 tasks

--- session_2/long_rivers.py
+++ session_2/long_rivers.py
@@ -9,9 +9,52 @@ rivers = [
     {"name": "Amazon", "length": 3915}
 ]
 
-def print_river_name(rivers=[]):
+def length_of_list(rivers):
+    count = 0
+    for x in range(len(rivers)):
+        count += 1
+    return count
+
+#fuction results as variables
+length_of_list = length_of_list(rivers)
+
+def print_river_name(rivers):
     for item in rivers:
         if "name" in item:
-            print("{}".format(item["name"], end=" "))
+            print("\n", item["name"])
 
-print_river_name(rivers)
+def sum_of_length(rivers):
+    result = 0
+    for item in rivers:
+        if "length" in item:
+            result += item["length"]
+    return result
+
+total_length = sum_of_length(rivers)
+
+#print("Total length of rivers: ", total_length)
+
+def converter(x):
+    mile = x
+    kilometer = mile * 1.6
+    return kilometer
+
+#extentions
+
+def length_converter(rivers):
+    for item in rivers:
+        if "length" in item:
+            new_length = converter(item["length"])
+    return new_length
+
+result = length_converter(rivers)
+
+def find_river_name(rivers):
+    for item in rivers:
+        if "M" in item["name"]:
+            print("\nRiver names that start with 'M': \n {}".format(item["name"]))
+
+
+if __name__ == "__main__":
+    print("\nThere are {} rivers  on this list.\n".format(length_of_list))
+    print("\nThe Total length of the {} rivers, is {} in miles\n".format(length_of_list, total_length, result))
